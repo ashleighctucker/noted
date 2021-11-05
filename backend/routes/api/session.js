72cd455863cf80b66b8e35c02fca2987c47ab52a
Ash Tucker <ashleighctucker@gmail.com>
2021-11-04 21:37:16 -0700

    session get route

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -6,6 +6,15 @@ const { User } = require('../../db/models');
 
 const router = express.Router();
 
+router.get('/', restoreUser, (req, res) => {
+  const { user } = req;
+  if (user) {
+    return res.json({
+      user: user.toSafeObject(),
+    });
+  } else return res.json({});
+});
+
 router.post(
   '/',
   asyncHander(async (req, res, next) => {
@@ -27,7 +36,5 @@ router.delete('/', (_req, res) => {
   res.clearCookie('token');
   return res.json({ message: 'logout sucess' });
 });
-
-
 
 module.exports = router;
