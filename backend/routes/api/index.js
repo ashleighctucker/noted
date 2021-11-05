ef106982536c7a778af6c6e99ddb33253b19bd2b
Ash Tucker <ashleighctucker@gmail.com>
2021-11-04 21:11:40 -0700

    added tests for other auth methods

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const asyncHandler = require('express-async-handler');
 
-const { setTokenCookie } = require('../../utils/auth.js');
+const {
+  setTokenCookie,
+  restoreUser,
+  requireAuth,
+} = require('../../utils/auth.js');
 const { User } = require('../../db/models');
 
 const router = express.Router();
@@ -10,7 +14,7 @@ router.post('/test', (req, res) => {
   return res.json({ requestBody: req.body });
 });
 
-//test set token cookie func
+//test set token cookie method
 router.get(
   '/set-token-cookie',
   asyncHandler(async (req, res) => {
@@ -23,4 +27,15 @@ router.get(
     return res.json({ user });
   })
 );
+
+//test restore user func
+router.get('/restore-user', restoreUser, (req, res) => {
+  return res.json(req.user);
+});
+
+//test require auth
+router.get('/require-auth', requireAuth, (req, res) => {
+  return res.json(req.user);
+});
+
 module.exports = router;
