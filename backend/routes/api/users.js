f2ca622883ce92fee9735efb665d93a99d9ef853
Ash Tucker <ashleighctucker@gmail.com>
2021-11-04 21:34:30 -0700

    added post new user route

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,4 +1,21 @@
-const express = require('express')
+const express = require('express');
+const asyncHandler = require('express-async-handler');
+
+const { setTokenCookie, requireAuth } = require('../../utils/auth');
+const { User } = require('../../db/models');
+
 const router = express.Router();
+
+router.post(
+  '/',
+  asyncHandler(async (req, res) => {
+    const { email, password, username } = req.body;
+    const newUser = await User.signup({ email, username, password });
+
+    await setTokenCookie(res, newUser);
+
+    return res.json({ newUser });
+  })
+);
 
 module.exports = router;
