3741dd7967dca764f7a848e8e805b5cb98c88455
Ash Tucker <ashleighctucker@gmail.com>
2021-11-08 11:04:39 -0800

    notes test seeder

---
+++ backend/db/seeders/20211108185748-testNotes.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert(
+      'Notes',
+      [
+        {
+          userId: 1,
+          title: 'Test note one',
+          content: 'Test note one content',
+        },
+        {
+          userId: 1,
+          title: 'Test note two',
+          content: 'Test note two content',
+        },
+        {
+          userId: 1,
+          title: 'Test note three',
+          content: 'Test note three content',
+        },
+      ],
+      {}
+    );
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('Notes', null, {});
+  },
+};
