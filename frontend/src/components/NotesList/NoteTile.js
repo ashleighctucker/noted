0d50d374e071c8c7d8beffad4c281692c7c07694
Ash Tucker <ashleighctucker@gmail.com>
2021-11-10 11:48:33 -0800

    highlight selected note

--- frontend/src/components/NotesList/NoteTile.js
+++ frontend/src/components/NotesList/NoteTile.js
@@ -1,12 +1,15 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink, useParams } from 'react-router-dom';
 
 const NoteTile = ({ note }) => {
+  const { noteId } = useParams();
+
+  const selected = +noteId === note.id;
   return (
     <NavLink
       className="nav-link"
       to={`/notebooks/${note.notebookId}/notes/${note.id}/edit`}
     >
-      <div className="note-tile">
+      <div className={selected ? 'note-tile-selected' : 'note-tile'}>
         <p>{note.title}</p>
         {/* <p>{note.content}</p> */}
       </div>
