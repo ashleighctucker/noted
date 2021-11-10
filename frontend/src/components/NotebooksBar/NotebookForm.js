70d3171a34582f54b4fcf3a6a0246bfcf76ed500
Ash Tucker <ashleighctucker@gmail.com>
2021-11-10 11:44:58 -0800

    highlight selected notebook

--- frontend/src/components/NotebooksBar/NotebookForm.js
+++ frontend/src/components/NotebooksBar/NotebookForm.js
@@ -1,7 +1,69 @@
-
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import { addNotebook } from '../../store/notebooks';
 
 const NotebookForm = () => {
-  return null;
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const [title, setTitle] = useState('');
+  const [photoUrl, setPhotoUrl] = useState('');
+  const [errors, setErrors] = useState([]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setErrors([]);
+    const notebook = await dispatch(addNotebook(title, photoUrl)).catch(
+      async (res) => {
+        const data = await res.json();
+        if (data && data.errors) {
+          const filteredErrors = data.errors.filter(
+            (error) => error !== 'Invalid value'
+          );
+          setErrors(filteredErrors);
+        }
+      }
+    );
+    if (notebook) {
+      history.push(`/notebooks/${notebook.id}/notes/new`);
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="title">Title</label>
+          <input
+            name="title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="photoUrl">Notbook Cover Photo</label>
+          <input
+            name="photoUrl"
+            value={photoUrl}
+            placeholder="Insert a photo URL"
+            onChange={(e) => setPhotoUrl(e.target.value)}
+          />
+        </div>
+        <div>
+          <div className="error-div">
+            <p className="user-form-errors">
+              {errors.map((error, i) => (
+                <span key={i}>{error}</span>
+              ))}
+            </p>
+          </div>
+          <button type="submit">
+            <i className="far fa-save"></i> Save
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default NotebookForm;
--- frontend/src/components/NotebooksBar/NotebookTile.js
+++ frontend/src/components/NotebooksBar/NotebookTile.js
@@ -1,14 +1,29 @@
-import { useHistory } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
 
 const NotebookTile = ({ notebook }) => {
+  const { notebookId } = useParams();
   const history = useHistory();
   const handleClick = () => {
     history.push(`/notebooks/${notebook.id}/notes/new`);
   };
 
+  const selected = +notebookId === notebook.id;
+
   return (
-    <div className="notebook-tile" key={notebook.id} onClick={handleClick}>
-      <img className="tile-image" src={notebook.photoUrl} alt="test" />
+    <div
+      className={selected ? 'notebook-tile-selected' : 'notebook-tile'}
+      key={notebook.id}
+      onClick={handleClick}
+    >
+      <img
+        className="tile-image"
+        src={
+          notebook.photoUrl
+            ? notebook.photoUrl
+            : 'https://images.unsplash.com/photo-1602629978851-125a3b62f3d8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80'
+        }
+        alt="test"
+      />
       <p>{notebook.title}</p>
     </div>
   );
