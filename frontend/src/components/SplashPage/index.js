2e52026cd5a035a04cce14b167b5713c8bf1dc5a
Ash Tucker <ashleighctucker@gmail.com>
2021-11-11 11:35:24 -0800

    splashpage done

--- frontend/src/components/SplashPage/index.js
+++ frontend/src/components/SplashPage/index.js
@@ -1,12 +1,30 @@
 import { NavLink } from 'react-router-dom';
+import './SplashPage.css';
 
 const SplashPage = () => {
   return (
-    <div>
-      <div>
-        <p>
-          Organize your life with <span>noted.</span>
+    <div id="spash-page-container">
+      <div className="splash-page-containers">
+        <p id="opener">
+          Organize your life... with <span id="logo-span">noted.</span>
         </p>
+        <p id="opener-2">
+          Plan your next big project, create a quick to-do list, or anything
+          in-between.
+        </p>
+        <NavLink to="/signup">
+          <button id="splash-signup-button">Signup for free</button>
+        </NavLink>
+        <NavLink id="login-redirect" to="/login">
+          Already have an account? Login
+        </NavLink>
+      </div>
+      <div className="splash-page-containers">
+        <img
+          id="notebook-image"
+          src="https://i.imgur.com/WPBPVC7.png"
+          alt="notebook"
+        ></img>
       </div>
     </div>
   );
