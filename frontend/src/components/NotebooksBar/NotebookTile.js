252671d3b82f88b346630f5132eaee933fd6a444
Ash Tucker <ashleighctucker@gmail.com>
2021-11-12 08:41:13 -0800

    changed default photo

--- frontend/src/components/NotebooksBar/NotebookTile.js
+++ frontend/src/components/NotebooksBar/NotebookTile.js
@@ -46,11 +46,7 @@ const NotebookTile = ({ notebook }) => {
       <div onClick={handleClick}>
         <img
           className="tile-image"
-          src={
-            photoUrl
-              ? photoUrl
-              : 'https://images.unsplash.com/photo-1602629978851-125a3b62f3d8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80'
-          }
+          src={photoUrl ? photoUrl : 'https://i.imgur.com/WPBPVC7.png'}
           alt="test"
         />
         <p>{title}</p>
