60d48bc3d4ef12caa982a5a5e549caaac2d9274a
Ash Tucker <ashleighctucker@gmail.com>
2021-11-14 16:14:48 -0800

    mobile changes

--- frontend/src/components/NotebooksBar/NotebookForm.js
+++ frontend/src/components/NotebooksBar/NotebookForm.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { useDispatch} from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import { addNotebook } from '../../store/notebooks';
 
